78e8ae54c01419d443905faba1fa2fb877672d51
eric cherin <eric.cherin@gmail.com>
2020-11-01 14:44:49 -0800

    added build folder

---
+++ precache-manifest.d3a591db1b47f5f3587492eb916fbeaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfba472cbd59aca5849d9e5f210937fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a6b622f78355ea8d49d",
+    "url": "/static/css/main.091b28ec.chunk.css"
+  },
+  {
+    "revision": "5381e8fc30c2c68af348",
+    "url": "/static/js/2.b4e3e591.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b4e3e591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6b622f78355ea8d49d",
+    "url": "/static/js/main.004e3ef7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
